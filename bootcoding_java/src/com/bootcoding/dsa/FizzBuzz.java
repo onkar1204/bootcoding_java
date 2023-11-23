a685050c81914e07a11dc5f92393d865b7a0791d
onkar1204 <114867129+onkar1204@users.noreply.github.com>
2023-11-23 17:43:03 +0530

    Update FizzBuzz.java
    i add a output message for divisible numbers

--- bootcoding_java/src/com/bootcoding/dsa/FizzBuzz.java
+++ bootcoding_java/src/com/bootcoding/dsa/FizzBuzz.java
@@ -14,7 +14,7 @@ public class FizzBuzz {
             } else if (i % 5 == 0) {
                 System.out.println("Buzz");
             }else{
-                    System.out.println("" + i);
+                    System.out.println("Nothing" + i);
                 }
                 
             }
